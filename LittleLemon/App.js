b03cd3c50b56c85b2c0beceff338ef1dc4f7655d
GitPit <rifflifter@gmail.com>
2023-02-11 12:37:07 -0500

    updated header flex to 0.3

--- LittleLemon/App.js
+++ LittleLemon/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import WelcomeScreen from "./pages/WelcomeScreen";
--- LittleLemon/components/LittleLemonHeader.js
+++ LittleLemon/components/LittleLemonHeader.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 export default function LittleLemonHeader() {
     return (
-      <View style={{ flex: 0.2, backgroundColor: '#F4CE14' }}>
+      <View style={{ flex: 0.3, backgroundColor: '#F4CE14' }}>
         <Text style={{ padding: 40, fontSize: 30, color: 'black' }}  numberOfLines={3}>Welcome to
         <Text style={{ fontWeight: 'bold'}}>Little Lemon</Text>
         </Text>
