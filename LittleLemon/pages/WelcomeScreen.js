da85f28e388b6b93a515f4bab1d834843b5cdfde
GitPit <rifflifter@gmail.com>
2023-02-11 12:19:15 -0500

    updated in-line styles for text

--- LittleLemon/pages/WelcomeScreen.js
+++ LittleLemon/pages/WelcomeScreen.js
@@ -4,9 +4,9 @@ import { Text, View } from 'react-native';
 export default function WelcomeScreen() {
     return (
       <View style={{ flex: 0.4, justifyContent: 'center' }}>
-        <Text style={{fontWeight: 'bold', fontSize: 20, color: 'white', textAlign: 'center'}}>Welcome to Little Lemon
+        <Text style={{fontWeight: 'bold', fontSize: 20, color: 'white', textAlign: 'center', paddingBottom: 20 }}>Welcome to Little Lemon
         </Text>
-        <Text style={{ fontSize: 14 }}>Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!</Text>
+        <Text style={{ fontSize: 14, color: 'white', textAlign: 'center' }}>Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!</Text>
       </View>
     );
   }
