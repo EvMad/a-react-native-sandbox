877840462e084462b64cbbc84a25faa6029eb563
GitPit <rifflifter@gmail.com>
2023-02-11 11:44:38 -0500

    export default function LittleLemonFooter()

--- LittleLemon/components/LittleLemonFooter.js
+++ LittleLemon/components/LittleLemonFooter.js
@@ -1,2 +1,11 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+
+export default function LittleLemonFooter() {
+    return (
+      <View>
+        <Text>All rights reserved by Little Lemon, 2023</Text>
+        
+      </View>
+    );
+  }
