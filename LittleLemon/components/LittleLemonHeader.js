100bc06220d99ba12ce1c8c28400d94cd7f85739
GitPit <rifflifter@gmail.com>
2023-02-11 11:40:37 -0500

    export default function LittleLemonHeader()

--- LittleLemon/components/LittleLemonHeader.js
+++ LittleLemon/components/LittleLemonHeader.js
@@ -1,2 +1,11 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+
+export default function LittleLemonHeader() {
+    return (
+      <View>
+        <Text>Little Lemon Restaurant</Text>
+        
+      </View>
+    );
+  }
