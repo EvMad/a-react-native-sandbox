f2e08cf164e3a3e6e0bcb8a7f8f6cac535f31ba7
GitPit <rifflifter@gmail.com>
2023-02-11 12:33:59 -0500

    changed WelcomeScreen View flex

--- LittleLemon/pages/WelcomeScreen.js
+++ LittleLemon/pages/WelcomeScreen.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 export default function WelcomeScreen() {
     return (
-      <View style={{ flex: 0.4, justifyContent: 'center' }}>
+      <View style={{ flex: 1 }}>
         <Text style={{fontWeight: 'bold', fontSize: 30, color: 'white', textAlign: 'center', padding: 40 }}>Welcome to Little Lemon
         </Text>
         <Text style={{ fontSize: 24, color: 'white', textAlign: 'center', padding: 20 }}>Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!</Text>
