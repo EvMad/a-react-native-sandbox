f8fef4e4073c3b1e579f420c6d071654b850c475
GitPit <rifflifter@gmail.com>
2023-02-11 11:47:54 -0500

    added in-line styling to App.js <View>'s

--- LittleLemon/App.js
+++ LittleLemon/App.js
@@ -6,10 +6,10 @@ import LittleLemonFooter from "./components/LittleLemonFooter";
 export default function App() {
   return (
     <>
-        <View>
+    <View style={{flex: 1, backgroundColor: '#495E57'}}>
       <LittleLemonHeader />
     </View>
-     <View>
+    <View style={{ backgroundColor: '#495E57' }}>
      <LittleLemonFooter />
    </View>
     </>
