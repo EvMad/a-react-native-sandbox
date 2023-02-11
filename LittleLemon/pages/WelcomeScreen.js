38628a40ecbfbf7aa0164c41f667a2d4086374e0
GitPit <rifflifter@gmail.com>
2023-02-11 12:05:29 -0500

    added text to WelcomeScreen

--- LittleLemon/pages/WelcomeScreen.js
+++ LittleLemon/pages/WelcomeScreen.js
@@ -4,8 +4,9 @@ import { Text, View } from 'react-native';
 export default function WelcomeScreen() {
     return (
       <View>
-       
-        
+        <Text>Welcome to Little Lemon
+            <Text>Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!</Text>
+        </Text>
       </View>
     );
   }
