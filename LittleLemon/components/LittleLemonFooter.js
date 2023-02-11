eec4ad2d639a2694674c8136fc35b9a0a506b528
GitPit <rifflifter@gmail.com>
2023-02-11 11:46:47 -0500

    added in-line styling to footer

--- LittleLemon/components/LittleLemonFooter.js
+++ LittleLemon/components/LittleLemonFooter.js
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native';
 
 export default function LittleLemonFooter() {
     return (
-      <View>
-        <Text>All rights reserved by Little Lemon, 2023</Text>
+      <View style={{ backgroundColor: '#F4CE14', marginBottom: 10}}>
+        <Text style={{fontSize: 18, color: 'black', textAlign: 'center'}}>All rights reserved by Little Lemon, 2023</Text>
         
       </View>
     );
