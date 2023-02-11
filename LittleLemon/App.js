3e5734f16c8df352ecf3c6d311705f396257b81b
GitPit <rifflifter@gmail.com>
2023-02-11 11:41:33 -0500

    imported header

--- LittleLemon/App.js
+++ LittleLemon/App.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { View } from 'react-native';
+import LittleLemonHeader from "./components/LittleLemonHeader";
 
 export default function App() {
   return (
     <View>
-      
+      <LittleLemonHeader />
      
     </View>
   );
