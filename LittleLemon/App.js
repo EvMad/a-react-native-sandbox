ce8676a2b77988622284fd92f22859d4249e6a24
GitPit <rifflifter@gmail.com>
2023-02-11 11:37:09 -0500

    removed <Text/> from App.js

--- LittleLemon/App.js
+++ LittleLemon/App.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
     <View>
-      <Text>Open up App.js to start working on your app!</Text>
+      
      
     </View>
   );
