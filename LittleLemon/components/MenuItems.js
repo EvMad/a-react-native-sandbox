5842d3bbe7daa2134173b08298623873f93b9849
GitPit <rifflifter@gmail.com>
2023-02-13 13:37:27 -0500

    reformatted items in menuItemsToDisplay array

--- LittleLemon/components/MenuItems.js
+++ LittleLemon/components/MenuItems.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, FlatList } from 'react-native';
 
 const green = '#495E57';
 const yellow = '#F4CE14';
 
 const menuItemsToDisplay = [
-    'Hummus \n Moutabal \n Falafel \n Marinated Olive \n Kafta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta',
+    { name: "Hummus", id: '0'},
+    { name: "Moutabal", id: '1'},
+    { name: "Falafel", id: '2'},
+    { name: "Marinated Olives", id: '3'},
+    { name: "Eggplant Salad", id: '4'},
+    { name: "Kofta", id: '5'},
+    { name: "Lentil Burger", id: '6'},
+    { name: "Smoked Salmon", id: '7'},
+    { name: "Kofta Burger", id: '8'},
+    { name: "Turkish Kebab", id: '9'},
+    { name: "Fries", id: '10'},
+    { name: "Buttered Rice", id: '11'},
+    { name: "Bread Sticks", id: '12'},
+    { name: "Pita Pocket", id: '13'},
+    { name: "Lentil Soup", id: '14'},
+    { name: "Greek Salad", id: '15'},
+    { name: "Rice Pilaf", id: '16'},
+    { name: "Baklava", id: '17'},
+    { name: "Tartufo", id: '18'},
+    { name: "Tiramisu", id: '19'},
+    { name: "Panna Cotta", id: '20'},
 ];
 
 const MenuItems = () => {
