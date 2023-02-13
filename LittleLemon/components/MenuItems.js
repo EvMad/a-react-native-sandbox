c028f984d68111a51bbe0b300b313dff4aad6992
GitPit <rifflifter@gmail.com>
2023-02-13 11:34:09 -0500

    added more text to menu items array

--- LittleLemon/components/MenuItems.js
+++ LittleLemon/components/MenuItems.js
@@ -6,7 +6,7 @@ const green = '#495E57';
 const yellow = '#F4CE14';
 
 const menuItemsToDisplay = [
-    'Hummus \n Moutabal \n Falafel \n Marinated Olive \n Kafta \n Eggplant Salad \n Lentil Burger',
+    'Hummus \n Moutabal \n Falafel \n Marinated Olive \n Kafta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta',
 ];
 
 const MenuItems = () => {
