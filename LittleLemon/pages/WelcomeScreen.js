1a2ed7247c96972cf8dc23190cf451f265c12ce4
GitPit <rifflifter@gmail.com>
2023-02-13 12:42:24 -0500

    container: {} / removed <View> wrapper

--- LittleLemon/pages/WelcomeScreen.js
+++ LittleLemon/pages/WelcomeScreen.js
@@ -3,22 +3,23 @@ import { Text, View, ScrollView, StyleSheet } from 'react-native';
 
 export default function WelcomeScreen() {
     return (
-      <View style={{ flex: 1 }}>
         <ScrollView 
         indicatorStyle={"white"}
               horizontal={false}  
-                style={{
-                padding: 40,
-                backgroundColor: '#495E57',
-            }}>
+                style={styles.container}>
         <Text style={{fontWeight: 'bold', fontSize: 30, color: 'white', textAlign: 'center', padding: 40 }}>Welcome to Little Lemon
         </Text>
         <Text style={{ fontSize: 24, color: 'white', textAlign: 'center', padding: 20 }}>Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!</Text>
         </ScrollView>
-      </View>
     );
   }
 
   const styles = StyleSheet.create({
-    
+
+      container: {
+        flex: 1,
+        padding: 40,
+        backgroundColor: '#495E57',
+      }
+
   })
