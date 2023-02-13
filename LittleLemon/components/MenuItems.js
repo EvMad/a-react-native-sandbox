5cc5a106f25d01326bbf4885f3dadd98fc229c2e
GitPit <rifflifter@gmail.com>
2023-02-13 11:19:30 -0500

    created MenuItems component

---
+++ LittleLemon/components/MenuItems.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { View, Text, ScrollView } from 'react-native';
+
+const green = '#495E57';
+const yellow = '#F4CE14';
+
+const menuItemsToDisplay = [
+
+];
+
+const MenuItems = () => {
+    return null;
+};
+
+export default MenuItems;
