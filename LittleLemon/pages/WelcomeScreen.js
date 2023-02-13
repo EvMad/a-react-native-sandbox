ff56040f2c5996ab737c611f34bff64cd24f2fe2
GitPit <rifflifter@gmail.com>
2023-02-13 11:44:47 -0500

    added ScrollView to WelcomeScreen

--- LittleLemon/pages/WelcomeScreen.js
+++ LittleLemon/pages/WelcomeScreen.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 
 export default function WelcomeScreen() {
     return (
       <View style={{ flex: 1 }}>
+        <ScrollView>
         <Text style={{fontWeight: 'bold', fontSize: 30, color: 'white', textAlign: 'center', padding: 40 }}>Welcome to Little Lemon
         </Text>
         <Text style={{ fontSize: 24, color: 'white', textAlign: 'center', padding: 20 }}>Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!</Text>
+        </ScrollView>
       </View>
     );
   }
