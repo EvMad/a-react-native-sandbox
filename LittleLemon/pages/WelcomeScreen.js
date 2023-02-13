3ecd403cb00afae442eff3994dda683951d74b17
GitPit <rifflifter@gmail.com>
2023-02-13 12:44:08 -0500

    <Text style={styles.headerText}>

--- LittleLemon/pages/WelcomeScreen.js
+++ LittleLemon/pages/WelcomeScreen.js
@@ -7,7 +7,7 @@ export default function WelcomeScreen() {
         indicatorStyle={"white"}
               horizontal={false}  
                 style={styles.container}>
-        <Text style={{fontWeight: 'bold', fontSize: 30, color: 'white', textAlign: 'center', padding: 40 }}>Welcome to Little Lemon
+        <Text style={styles.headerText}>Welcome to Little Lemon
         </Text>
         <Text style={{ fontSize: 24, color: 'white', textAlign: 'center', padding: 20 }}>Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!</Text>
         </ScrollView>
@@ -20,6 +20,13 @@ export default function WelcomeScreen() {
         flex: 1,
         padding: 40,
         backgroundColor: '#495E57',
+      },
+      headerText: {
+        fontWeight: 'bold', 
+        fontSize: 30, 
+        color: 'white', 
+        textAlign: 'center', 
+        padding: 40,
       }
 
   })
