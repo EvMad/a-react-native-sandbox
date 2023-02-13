6ba21e8d3abef6f0bd23a941300ab97684bc7a10
GitPit <rifflifter@gmail.com>
2023-02-13 14:04:42 -0500

    attempted to define item.price

--- LittleLemon/components/MenuItems.js
+++ LittleLemon/components/MenuItems.js
@@ -6,40 +6,41 @@ const green = '#495E57';
 const yellow = '#F4CE14';
 
 const menuItemsToDisplay = [
-    { name: "Hummus", id: '1A'},
-    { name: "Moutabal", id: '2B'},
-    { name: "Falafel", id: '3C'},
-    { name: "Marinated Olives", id: '4D'},
-    { name: "Eggplant Salad", id: '5E'},
-    { name: "Kofta", id: '6F'},
-    { name: "Lentil Burger", id: '7G'},
-    { name: "Smoked Salmon", id: '8H'},
-    { name: "Kofta Burger", id: '9I'},
-    { name: "Turkish Kebab", id: '10J'},
-    { name: "Fries", id: '11K'},
-    { name: "Buttered Rice", id: '12L'},
-    { name: "Bread Sticks", id: '13M'},
-    { name: "Pita Pocket", id: '14N'},
-    { name: "Lentil Soup", id: '15O'},
-    { name: "Greek Salad", id: '16P'},
-    { name: "Rice Pilaf", id: '17Q'},
-    { name: "Baklava", id: '18R'},
-    { name: "Tartufo", id: '19S'},
-    { name: "Tiramisu", id: '20T'},
-    { name: "Panna Cotta", id: '21U'},
+    { name: "Hummus", price: '$5.00', id: '1A'},
+    { name: "Moutabal", price: '$5.00', id: '2B'},
+    { name: "Falafel", price: '$7.50', id: '3C'},
+    { name: "Marinated Olives", price: '$5.00', id: '4D'},
+    { name: "Eggplant Salad", price: '$8.50', id: '5E'},
+    { name: "Kofta", price: '$5.00', id: '6F'},
+    { name: "Lentil Burger", price: '$10.00', id: '7G'},
+    { name: "Smoked Salmon", price: '$14.00', id: '8H'},
+    { name: "Kofta Burger", price: '$11.00', id: '9I'},
+    { name: "Turkish Kebab", price: '$15.50', id: '10J'},
+    { name: "Fries", price: '$3.00', id: '11K'},
+    { name: "Buttered Rice", price: '$3.00', id: '12L'},
+    { name: "Bread Sticks", price: '$3.00', id: '13M'},
+    { name: "Pita Pocket", price: '$3.00', id: '14N'},
+    { name: "Lentil Soup", price: '$3.75', id: '15O'},
+    { name: "Greek Salad", price: '$6.00', id: '16P'},
+    { name: "Rice Pilaf", price: '$4.00', id: '17Q'},
+    { name: "Baklava", price: '$3.00', id: '18R'},
+    { name: "Tartufo", price: '$3.00', id: '19S'},
+    { name: "Tiramisu", price: '$5.00', id: '20T'},
+    { name: "Panna Cotta", price: '$5.00', id: '21U'},
 ];
 
-const Item = ({name}) => {
+const Item = ({name}, {price}) => {
     return (
         <View style={menuStyles.container}>
             <Text style={menuStyles.itemText}>{name}</Text>
+            <Text style={menuStyles.itemPrice}>{price}</Text>
         </View>
     );
 };
 
 const MenuItems = () => {
 
-    const renderItem = ({ item }) => <Item name={item.name} />;
+    const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
 
     return (
         <View style={menuStyles.container}>
@@ -69,6 +70,12 @@ const menuStyles = StyleSheet.create({
     },
     itemText: {
         fontSize: 24, 
-        color: yellow 
+        color: yellow,
+        justifyContent: 'flex-start',
+    },
+    itemPrice: {
+        fontSize: 24, 
+        color: yellow ,
+        justifyContent: 'flex-end',
     },
 });
