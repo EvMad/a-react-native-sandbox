5f4e626b343b84a1032457b5fb4a2ff4139bdc2d
GitPit <rifflifter@gmail.com>
2023-02-14 14:06:07 -0500

    const renderItem = ({item}) => <Item name={item} /

--- LittleLemon/components/MenuItems.js
+++ LittleLemon/components/MenuItems.js
@@ -72,20 +72,22 @@ const Footer = () => <Text style={menuStyles.footerText}>
     All Rights Reserved by Little Lemon 2023
 </Text>
 
-const Item = ({ name, price }) => {
+const Item = ({ name }) => {
     return (
         <View style={menuStyles.innerContainer}>
             <Text style={menuStyles.itemText}>{name}</Text>
-            <Text style={menuStyles.itemText}>{price}</Text>
+            {/* <Text style={menuStyles.itemText}>{price}</Text> */}
         </View>
     );
 };
 
 const MenuItems = () => {
 
+    const renderItem = ({item}) => <Item name={item} />
+
     return (
         <View style={menuStyles.container}>
-            <SectionList sections={menuItemsToDisplay}/>
+            <SectionList sections={menuItemsToDisplay} renderItem={renderItem}/>
         </View>
     );
 
