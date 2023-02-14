9d5048ab87745477d138149248cecc289ffdae16
GitPit <rifflifter@gmail.com>
2023-02-14 12:55:07 -0500

    updated menu items array for SectionList

--- LittleLemon/components/MenuItems.js
+++ LittleLemon/components/MenuItems.js
@@ -6,27 +6,62 @@ const green = '#495E57';
 const yellow = '#F4CE14';
 
 const menuItemsToDisplay = [
-    { name: "Hummus", price: '$5.00', id: '1A'},
-    { name: "Moutabal", price: '$5.00', id: '2B'},
-    { name: "Falafel", price: '$7.50', id: '3C'},
-    { name: "Marinated Olives", price: '$5.00', id: '4D'},
-    { name: "Eggplant Salad", price: '$8.50', id: '5E'},
-    { name: "Kofta", price: '$5.00', id: '6F'},
-    { name: "Lentil Burger", price: '$10.00', id: '7G'},
-    { name: "Smoked Salmon", price: '$14.00', id: '8H'},
-    { name: "Kofta Burger", price: '$11.00', id: '9I'},
-    { name: "Turkish Kebab", price: '$15.50', id: '10J'},
-    { name: "Fries", price: '$3.00', id: '11K'},
-    { name: "Buttered Rice", price: '$3.00', id: '12L'},
-    { name: "Bread Sticks", price: '$3.00', id: '13M'},
-    { name: "Pita Pocket", price: '$3.00', id: '14N'},
-    { name: "Lentil Soup", price: '$3.75', id: '15O'},
-    { name: "Greek Salad", price: '$6.00', id: '16P'},
-    { name: "Rice Pilaf", price: '$4.00', id: '17Q'},
-    { name: "Baklava", price: '$3.00', id: '18R'},
-    { name: "Tartufo", price: '$3.00', id: '19S'},
-    { name: "Tiramisu", price: '$5.00', id: '20T'},
-    { name: "Panna Cotta", price: '$5.00', id: '21U'},
+
+    {
+        title: 'Appetizers',
+        data: [
+            "Hummus",
+            "Moutabal",
+            "Falafel",
+            "Marinated Olives",
+            "Kofta",
+            "Eggplant Salad",
+        ],
+    },
+    {
+        title: "Main Dishes",
+        data: ["Lentil Burger", "Smoked Salmon", "Kofta Burger", "Turkish Kebab"],
+    },
+    {
+        title: "Sides",
+        data: [
+            "Fries",
+            "Buttered Rice",
+            "Bread Sticks",
+            "Pita Pocket",
+            "Lentil Soup",
+            "Greek Salad",
+            "Rice Pilaf",
+        ],
+    },
+    {
+        title: "Desserts",
+        data: [ "Baklava", "Tartufo", "Tiramisu", "Panna Cotta"],
+    },
+
+
+
+    // { name: "Hummus", price: '$5.00', id: '1A'},
+    // { name: "Moutabal", price: '$5.00', id: '2B'},
+    // { name: "Falafel", price: '$7.50', id: '3C'},
+    // { name: "Marinated Olives", price: '$5.00', id: '4D'},
+    // { name: "Eggplant Salad", price: '$8.50', id: '5E'},
+    // { name: "Kofta", price: '$5.00', id: '6F'},
+    // { name: "Lentil Burger", price: '$10.00', id: '7G'},
+    // { name: "Smoked Salmon", price: '$14.00', id: '8H'},
+    // { name: "Kofta Burger", price: '$11.00', id: '9I'},
+    // { name: "Turkish Kebab", price: '$15.50', id: '10J'},
+    // { name: "Fries", price: '$3.00', id: '11K'},
+    // { name: "Buttered Rice", price: '$3.00', id: '12L'},
+    // { name: "Bread Sticks", price: '$3.00', id: '13M'},
+    // { name: "Pita Pocket", price: '$3.00', id: '14N'},
+    // { name: "Lentil Soup", price: '$3.75', id: '15O'},
+    // { name: "Greek Salad", price: '$6.00', id: '16P'},
+    // { name: "Rice Pilaf", price: '$4.00', id: '17Q'},
+    // { name: "Baklava", price: '$3.00', id: '18R'},
+    // { name: "Tartufo", price: '$3.00', id: '19S'},
+    // { name: "Tiramisu", price: '$5.00', id: '20T'},
+    // { name: "Panna Cotta", price: '$5.00', id: '21U'},
 ];
 
 const Separator = () => <View style={menuStyles.separator} />;
