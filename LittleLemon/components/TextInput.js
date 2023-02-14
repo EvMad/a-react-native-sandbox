3a7c38641e8507100c6d0764701e423e38ad9b54
GitPit <rifflifter@gmail.com>
2023-02-14 15:03:46 -0500

    updated styles for TextInput

--- LittleLemon/components/TextInput.js
+++ LittleLemon/components/TextInput.js
@@ -1,3 +1,37 @@
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 
+export default function FeedbackForm() {
+    return null;
+};
+
+const styles =StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#495E57'
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        fontSize: 16,
+        borderColor: '#EDEFEE',
+        backgroundColor: '#F4CE14',
+    },
+    messageInput: {
+        height: 100,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        fontSize: 16,
+        backgroundColor: '#F4CE14',
+    },
+    infoSection: {
+        fontSize: 20,
+        padding: 20,
+        marginVertical: 8,
+        color: '#EDEFEE',
+        textAlign: 'center',
+    },
+})
