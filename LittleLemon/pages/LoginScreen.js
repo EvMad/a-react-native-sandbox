c5c8fcaec276960f19a5d8513364467daca310be
GitPit <rifflifter@gmail.com>
2023-02-15 11:32:08 -0500

    added TextInput for username and password

--- LittleLemon/pages/LoginScreen.js
+++ LittleLemon/pages/LoginScreen.js
@@ -1,2 +1,18 @@
 import * as React from 'react';
-import { ScrollView, Text, StyleSheet} from 'react-native';
+import { ScrollView, Text, StyleSheet } from 'react-native';
+
+
+const LoginScreen = () => {
+    const [userName, onChangeUserName] = React.useState('');
+    const [password, onChangePassword] = React.useState('');
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+        <Text style={styles.regularText}>Login to continue </Text>
+        <TextInput value={userName} onChangeText={onChangeUserName} style={styles.input} placeholder="username" />
+        <TextInput value={password} onChangeText={onChangePassword} style={styles.input} placeholder="password" />
+      </ScrollView>
+    );
+  }
+
+  export default LoginScreen;
