d41c2a98dccf99720987f9ab4a1103ab082bca6c
GitPit <rifflifter@gmail.com>
2023-02-16 09:40:57 -0500

    import Image

--- LittleLemon/pages/WelcomeScreen.js
+++ LittleLemon/pages/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Image } from 'react-native';
 
 export default function WelcomeScreen() {
     return (
