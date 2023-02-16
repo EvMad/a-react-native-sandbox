e823114c1fe4d05383034760c4c8150b1cf4ec6c
GitPit <rifflifter@gmail.com>
2023-02-16 09:45:21 -0500

    logo: {}

--- LittleLemon/pages/WelcomeScreen.js
+++ LittleLemon/pages/WelcomeScreen.js
@@ -7,7 +7,7 @@ export default function WelcomeScreen() {
         indicatorStyle={"white"}
               horizontal={false}  
                 style={styles.container}>
-                  <Image source={require("")} />
+                  <Image source={require("")} style={styles.logo} />
         <Text style={styles.headerText}>Welcome to Little Lemon
         </Text>
         <Text style={styles.bodyText}>Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!</Text>
@@ -22,6 +22,11 @@ export default function WelcomeScreen() {
         padding: 40,
         backgroundColor: '#333333',
        
+      },
+      logo: {
+        height: 100,
+        width: 300,
+        resizeMode: 'contain',
       },
       headerText: {
         fontWeight: 'bold', 
