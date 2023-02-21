2049a607d60084a4cc6d4961bd74bd760ff77ebc
GitPit <rifflifter@gmail.com>
2023-02-21 15:42:08 -0500

    initialRouteName='Welcome'

--- LittleLemon/App.js
+++ LittleLemon/App.js
@@ -33,7 +33,7 @@ export default function App() {
      <LittleLemonHeader />
 
 
-     <Drawer.Navigator useLegacyImplementation screenOptions={{ drawerPosition: "right" }}>
+     <Drawer.Navigator useLegacyImplementation initialRouteName='Welcome' screenOptions={{ drawerPosition: "right" }}>
       <Drawer.Screen name="Welcome" component={WelcomeScreen} />
       <Drawer.Screen name="Menu" component={MenuItems} />
      </Drawer.Navigator>
