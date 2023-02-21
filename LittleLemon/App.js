855fa5d7fe3972aa4c493fee5e1331e5f9ff0ef3
GitPit <rifflifter@gmail.com>
2023-02-21 15:16:37 -0500

    const Drawer = createDrawerNavigator();

--- LittleLemon/App.js
+++ LittleLemon/App.js
@@ -14,6 +14,7 @@ import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 function LogoTitle() {
   return <Image source={require('./img/LittleLemonLogo.png')} style={{
